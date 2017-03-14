1bacd34365984e8c56c3aeeefcfe8ec99d11d2fd
Zach Siglin <zsiglin@gmail.com>
2017-03-13 22:05:12 -0600

    Add bodyParser Express middleware

--- api.js
+++ api.js
@@ -1,7 +1,11 @@
 // dependencies
 const express = require('express');
+const bodyParser = require('body-parser');
 const redis = require('redis');
 const api = express();
+
+// express middleware for JSON parsing
+api.use(bodyParser.json());
 
 api.post('/words.json', (req, res) => {
 
@@ -19,4 +23,5 @@ api.delete('/words.json', (req, res) => {
 
 });
 
+// start server on port 3000
 api.listen(3000);
